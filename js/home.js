d95019981a5111faa1bcb2d71b1aaf1dc4cfbaf4
Hasan1971f <mdhasanurrahmanhasan101@gmail.com>
2024-09-25 21:02:37 +0600

    added a home.js file

---
+++ js/home.js
@@ -0,0 +1,6 @@
+const blog = document.getElementById('blog')
+blog.addEventListener('click',function (event) {
+    event.preventDefault()
+    console.log("home clicked")
+    window.location.href="index.html"
+})
